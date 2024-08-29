13266b52c185011e8e940d2fcd2b99700a43be60
Youssef-001 <youssefsamir432@gmail.com>
2024-08-29 09:25:44 +0300

    Add info page layout
    - Add layout for main page, with explaining rules and what each color means
    - Add buttons for multiplayer and Bot

--- src/index.js
+++ src/index.js
@@ -2,176 +2,191 @@ import Player from "./js/Player";
 import { renderBoard, updateCell, shipSink, renderWinner } from "./js/Render";
 import style from "./styles/style.css";
 import computerMove from "./js/utility";
-let player1 = new Player();
+function playGame(bot) {
+  document.querySelector(".page").remove();
+  let player1 = new Player();
+  renderBoard(player1);
+  let computer;
+  if (bot) computer = new Player(true);
+  else computer = new Player();
+  renderBoard(computer);
 
-renderBoard(player1);
-let computer = new Player(true);
-console.log(player1.board.board);
-renderBoard(computer);
+  let turn = true;
 
-let turn = true;
+  function Start(player1, computer) {
+    let board_div1;
+    let board_div2;
 
-function Game(player1, computer) {
-  let board_div1;
-  let board_div2;
+    board_div1 = document.querySelector(".board-2");
+    board_div2 = document.querySelector(".board-1");
 
-  board_div1 = document.querySelector(".board-2");
-  board_div2 = document.querySelector(".board-1");
+    let cells1 = [];
+    let cells2 = [];
 
-  let cells1 = [];
-  let cells2 = [];
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        let cord = `${i} ${j}`;
+        console.log("cord: ", cord);
+        let elem = board_div1.querySelector(`[id='${cord}']`);
+        let elem2 = board_div2.querySelector(`[id='${cord}']`);
 
-  for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < 10; j++) {
-      let cord = `${i} ${j}`;
-      console.log("cord: ", cord);
-      let elem = board_div1.querySelector(`[id='${cord}']`);
-      let elem2 = board_div2.querySelector(`[id='${cord}']`);
-
-      if (
-        computer.board.board[i][j] == 0 ||
-        !computer.board.isAttacked(computer, i, j)
-      )
-        cells1.push(elem);
-      if (
-        player1.board.board[i][j] == 0 ||
-        !player1.board.isAttacked(computer, i, j)
-      )
-        cells2.push(elem2);
+        if (
+          computer.board.board[i][j] == 0 ||
+          !computer.board.isAttacked(computer, i, j)
+        )
+          cells1.push(elem);
+        if (
+          player1.board.board[i][j] == 0 ||
+          !player1.board.isAttacked(computer, i, j)
+        )
+          cells2.push(elem2);
+      }
     }
-  }
-  function handleCellClick(e) {
-    let target;
+    function handleCellClick(e) {
+      let target;
 
-    if (e.target == undefined) target = e;
-    else target = e.target;
+      if (e.target == undefined) target = e;
+      else target = e.target;
 
-    let currentPlayer;
-    let id;
-    if (turn == true) {
-      currentPlayer = player1;
-      id = 1;
-    } else {
-      currentPlayer = computer;
-      id = 2;
-    }
+      let currentPlayer;
+      let id;
+      if (turn == true) {
+        currentPlayer = player1;
+        id = 1;
+      } else {
+        currentPlayer = computer;
+        id = 2;
+      }
 
-    console.log(target.className);
-    let cord = target.id.split(" ");
-    let attackedCell = currentPlayer.board.isAttacked(
-      currentPlayer,
-      cord[0],
-      cord[1]
-    );
+      console.log(target.className);
+      let cord = target.id.split(" ");
+      let attackedCell = currentPlayer.board.isAttacked(
+        currentPlayer,
+        cord[0],
+        cord[1]
+      );
 
-    cord[0] = parseInt(cord[0]);
-    cord[1] = parseInt(cord[1]);
+      cord[0] = parseInt(cord[0]);
+      cord[1] = parseInt(cord[1]);
 
-    if (currentPlayer.board.board[cord[0]][cord[1]] == 0) {
-      currentPlayer.board.board[cord[0]][cord[1]] = -1;
-      turn = !turn;
-      updateCell(id, target, currentPlayer, cord[0], cord[1]);
-      if (player1.isBot == true || computer.isBot == true) updateListeners2();
-      else updateListeners();
-    } else if (!attackedCell) {
-      currentPlayer.board.attacked_ship_cells.push([cord[0], cord[1]]);
-      let currentShip = currentPlayer.board.board[cord[0]][cord[1]];
-      currentShip.hit();
-      console.log(currentShip);
-      updateCell(id, target, currentPlayer, cord[0], cord[1]);
-      if (turn) id = 1;
-      else id = 2;
+      if (currentPlayer.board.board[cord[0]][cord[1]] == 0) {
+        currentPlayer.board.board[cord[0]][cord[1]] = -1;
+        turn = !turn;
+        updateCell(id, target, currentPlayer, cord[0], cord[1]);
+        if (player1.isBot == true || computer.isBot == true) updateListeners2();
+        else updateListeners();
+      } else if (!attackedCell) {
+        currentPlayer.board.attacked_ship_cells.push([cord[0], cord[1]]);
+        let currentShip = currentPlayer.board.board[cord[0]][cord[1]];
+        currentShip.hit();
+        console.log(currentShip);
+        updateCell(id, target, currentPlayer, cord[0], cord[1]);
+        if (turn) id = 1;
+        else id = 2;
 
-      if (currentShip.isSunk()) {
-        console.log("A ship has been destroyed");
-        currentShip.coordinates.forEach((c) => {
-          let cell = currentPlayer.board.getCell(id, c[0], c[1]);
-          updateCell(id, cell, currentPlayer, c[0], c[1]);
-        });
+        if (currentShip.isSunk()) {
+          console.log("A ship has been destroyed");
+          currentShip.coordinates.forEach((c) => {
+            let cell = currentPlayer.board.getCell(id, c[0], c[1]);
+            updateCell(id, cell, currentPlayer, c[0], c[1]);
+          });
 
-        // TODO: check if all ships has sank
-        if (currentPlayer.board.allSunk()) {
-          console.log(`player ${currentPlayer.id} won!!`);
-          renderWinner(id);
-          if (computer.isBot == true || player1.isBot == true) {
-            updateListeners2(true);
-          } else updateListeners(true, null);
+          // TODO: check if all ships has sank
+          if (currentPlayer.board.allSunk()) {
+            console.log(`player ${currentPlayer.id} won!!`);
+            renderWinner(id);
+            if (computer.isBot == true || player1.isBot == true) {
+              updateListeners2(true);
+            } else updateListeners(true, null);
+          }
         }
       }
     }
+
+    function updateListeners(flag = null, bot = null) {
+      if (flag) {
+        cells2.forEach((cell) => {
+          cell.removeEventListener("click", handleCellClick);
+        });
+
+        cells1.forEach((cell) => {
+          cell.removeEventListener("click", handleCellClick);
+        });
+        return;
+      }
+      if (turn) {
+        cells2.forEach((cell) => {
+          cell.removeEventListener("click", handleCellClick);
+        });
+
+        cells1.forEach((cell) => {
+          cell.addEventListener("click", handleCellClick);
+        });
+      } else {
+        cells1.forEach((cell) => {
+          cell.removeEventListener("click", handleCellClick);
+        });
+
+        cells2.forEach((cell) => {
+          cell.addEventListener("click", handleCellClick);
+        });
+      }
+    }
+
+    function updateListeners2(end = null) {
+      if (end == true) {
+        cells1.forEach((cell) => {
+          cell.removeEventListener("click", handleCellClick);
+        });
+
+        cells2.forEach((cell) => {
+          cell.removeEventListener("click", handleCellClick);
+        });
+        return;
+      }
+      if (turn) {
+        cells1.forEach((cell) => {
+          cell.addEventListener("click", handleCellClick);
+        });
+      } else {
+        cells1.forEach((cell) => {
+          cell.removeEventListener("click", handleCellClick);
+        });
+      }
+
+      if (computer.isBot === true && !turn) {
+        setTimeout(() => {
+          let ind = computerMove();
+          console.log(ind);
+          let coord = ind.toString().replace(",", " ");
+          let div1 = document.querySelector(".board-1");
+          let element = div1.querySelector(`[id='${coord}']`);
+
+          handleCellClick(element);
+
+          if (!turn) {
+            if (computer.board.allSunk()) {
+              updateListeners2(true);
+            } else updateListeners2();
+          }
+        }, 600);
+      }
+    }
+
+    updateListeners();
   }
 
-  function updateListeners(flag = null, bot = null) {
-    if (flag) {
-      cells2.forEach((cell) => {
-        cell.removeEventListener("click", handleCellClick);
-      });
-
-      cells1.forEach((cell) => {
-        cell.removeEventListener("click", handleCellClick);
-      });
-      return;
-    }
-    if (turn) {
-      cells2.forEach((cell) => {
-        cell.removeEventListener("click", handleCellClick);
-      });
-
-      cells1.forEach((cell) => {
-        cell.addEventListener("click", handleCellClick);
-      });
-    } else {
-      cells1.forEach((cell) => {
-        cell.removeEventListener("click", handleCellClick);
-      });
-
-      cells2.forEach((cell) => {
-        cell.addEventListener("click", handleCellClick);
-      });
-    }
-  }
-
-  function updateListeners2(end = null) {
-    if (end == true) {
-      cells1.forEach((cell) => {
-        cell.removeEventListener("click", handleCellClick);
-      });
-
-      cells2.forEach((cell) => {
-        cell.removeEventListener("click", handleCellClick);
-      });
-      return;
-    }
-    if (turn) {
-      cells1.forEach((cell) => {
-        cell.addEventListener("click", handleCellClick);
-      });
-    } else {
-      cells1.forEach((cell) => {
-        cell.removeEventListener("click", handleCellClick);
-      });
-    }
-
-    if (computer.isBot === true && !turn) {
-      setTimeout(() => {
-        let ind = computerMove();
-        console.log(ind);
-        let coord = ind.toString().replace(",", " ");
-        let div1 = document.querySelector(".board-1");
-        let element = div1.querySelector(`[id='${coord}']`);
-
-        handleCellClick(element);
-
-        if (!turn) {
-          if (computer.board.allSunk()) {
-            updateListeners2(true);
-          } else updateListeners2();
-        }
-      }, 600);
-    }
-  }
-
-  updateListeners();
+  Start(player1, computer);
 }
+let multiplayerButton = document.querySelector(".player2");
+let botButton = document.querySelector(".bot");
 
-Game(player1, computer);
+multiplayerButton.addEventListener("click", () => {
+  playGame();
+});
+
+botButton.addEventListener("click", () => {
+  playGame(true);
+});
+
+// playGame(true);
